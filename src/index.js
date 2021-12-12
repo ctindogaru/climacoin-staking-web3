21a3b8c7b1f58fd63a73dbb1fe5d576eb045c166
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-12-12 13:16:03 +0200

    format files and replace cliq with clc inside index html

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require('file-loader?name=[name].html!./index.html');
+require("file-loader?name=[name].html!./index.html");
 
 // import css files
-import './App.scss';
-import './css/base.css';
-import './css/style.css';
-import './css/util.css';
+import "./App.scss";
+import "./css/base.css";
+import "./css/style.css";
+import "./css/util.css";
 
 // import image files
-import './images/icons/favicon.ico';
-import './images/diamond.png';
-import './images/metamask-icon.png';
-import './images/walletconnect-icon.png';
+import "./images/icons/favicon.ico";
+import "./images/diamond.png";
+import "./images/metamask-icon.png";
+import "./images/walletconnect-icon.png";
 
 // import js files
-import './App.js';
-import './assets/web3.js';
+import "./App.js";
+import "./assets/web3.js";
